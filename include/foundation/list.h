4c0d3941ed5b66b38a27c957b491a137c3712568
Elian <eliandotta@gmail.com>
2026-08-19 01:16:42 -0300

    Added list files, structure and functions
    - Added list.h in include/foundation/.
    - Added list.c in src/foundation/.
    - Defined list node structure tNode in list.h.
    - Defined init and free list functions.
    - Defined insert at head, tail and ordered function.
    - Defined get head and tail functions.
    - Defined delete element by comparation.
    - Defined map and show functions.

---
+++ include/foundation/list.h
@@ -0,0 +1,29 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+
+typedef struct sNode 
+{
+    void *data;
+    unsigned dataSize;
+    struct sNode *next;
+}tNode;
+
+typedef tNode *tList;
+
+typedef (*CompareFunc)(const void *, const void *);
+typedef (*ActionFunc)(void *, void *);
+typedef (*ShowFunc)(const void *);
+
+void initList(tList *list);
+tError insertHead(tList *list, const void *data, unsigned dataSize);
+tError insertTail(tList *list, const void *data, unsigned dataSize);
+tError insertOrdered(tList *list, const void *data, unsigned dataSize, CompareFunc compare);
+tError getHead(tList *list, void *data, unsigned dataSize);
+tError getTail(tList *list, void *data, unsigned dataSize);
+tError deleteElement(tList *list, const void *data, unsigned dataSize, CompareFunc compare);
+tError mapList(tList *list, ActionFunc action, void *context);
+tError showList(tList *list, ShowFunc show);
+void freeList(tList *list);
+
+
+#endif // LIST_H_INCLUDED
